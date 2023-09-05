415b1e34fda39866661b54ff98c5a62d22d7872b
k4rimDev <kenanovkenan299@gmail.com>
2023-09-05 10:25:43 +0400

    Fix refresh token

--- apps/account/api/urls.py
+++ apps/account/api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from rest_framework_simplejwt.views import TokenRefreshView
+
 from apps.account.api import views
 
 
@@ -7,6 +9,6 @@ urlpatterns = [
     # Account API
     path('auth/token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_view'),
     path('auth/token/remove', views.CustomLogoutAPIView.as_view(), name='custom_logout'),
-    path('auth/token/refresh/', views.MyTokenRefreshView.as_view(), name='token_refresh_view'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh_view'),
     path('logout/blacklist/', views.BlacklistTokenUpdateView.as_view(), name='blacklist'),
 ]
