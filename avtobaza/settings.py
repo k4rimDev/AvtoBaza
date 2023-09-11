9358090a39886203e60e3d0de4564df1bac5254d
k4rimDev <kenanovkenan299@gmail.com>
2023-09-11 13:38:07 +0400

    :hammer: Fix host

--- avtobaza/settings.py
+++ avtobaza/settings.py
@@ -73,7 +73,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-    'apps.utils.middlewares.RequestLogMiddleware' # For logging and add to MongoDB
+    # 'apps.utils.middlewares.RequestLogMiddleware' # For logging and add to MongoDB
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
