4a2349f0bc3d4394a753f12d1164f7db68a5628c
k4rimDev <kenanovkenan299@gmail.com>
2023-09-25 18:05:45 +0400

    :hammer: Exclude celery and django password token from admin

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -2,6 +2,24 @@ from django.contrib import admin
 
 from apps.core import models
 
+from django_celery_beat.models import IntervalSchedule, PeriodicTask, ClockedSchedule, CrontabSchedule, \
+    SolarSchedule
+from django_celery_results.models import TaskResult, GroupResult
+
+from django_rest_passwordreset.models import ResetPasswordToken
+from django_rest_passwordreset.admin import ResetPasswordTokenAdmin
+
+admin.site.unregister(IntervalSchedule)
+admin.site.unregister(PeriodicTask)
+admin.site.unregister(ClockedSchedule)
+
+admin.site.unregister(TaskResult)
+admin.site.unregister(GroupResult)
+admin.site.unregister(CrontabSchedule)
+admin.site.unregister(SolarSchedule)
+
+
+admin.site.unregister(ResetPasswordToken)
 
 admin.site.register(models.Slider)
 admin.site.register(models.CampaignText)
